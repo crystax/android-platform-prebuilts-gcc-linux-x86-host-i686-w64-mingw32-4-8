cf46b91ed6ac124cae6443beb55439a9d595e515
Dmitry Moskalchuk <dm@crystax.net>
2014-07-16 17:31:29 +0400

    Initial commit of prebuilt mingw32 toolchain
    This is build of i686-w64-mingw32 toolchain based on
    https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/host/x86_64-w64-mingw32-4.8/
    repository. The only difference is adding --enable-sjlj-exceptions for i686 build.
    
    This build was built with:
    ./build-mingw64-toolchain.sh --gcc-version=4.8.3 --target-arch=i686

---
+++ lib/gcc/i686-w64-mingw32/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/build-mingw64-toolchain-crystax/src/gcc-4.8.3/configure --prefix=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/i686-w64-mingw32 --disable-shared --with-gmp=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/i686-w64-mingw32 --with-mpfr=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/i686-w64-mingw32 --with-mpc=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/i686-w64-mingw32 --target=i686-w64-mingw32 --enable-targets=all --enable-languages=c,c++ --with-sysroot=/tmp/build-mingw64-toolchain-crystax/install-x86_64-linux-gnu/i686-w64-mingw32 --enable-sjlj-exceptions";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu_32", "generic" }, { "cpu_64", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" } };
